10e8b7c701db22c385984bb1cac15c7e2bd0388f
Kirtiraj Jamnotiya <136007344+kirtiraj2215@users.noreply.github.com>
2024-08-28 23:08:15 +0530

    Updated control.ino
    Updated the control.ino with all detailed functions for the motion of motors

--- control/control.ino
+++ control/control.ino
@@ -1,6 +1,12 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
+// Motor control pins
+#define IN1 25   // Motor A direction control
+#define IN2 26   // Motor A direction control
+#define IN3 27   // Motor B direction control
+#define IN4 14   // Motor B direction control
+
 const char* ssid = "IIT_Bhilai";
 const char* password = "iitbhilai2023";
 
@@ -18,15 +24,77 @@ void setup() {
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/send", HTTP_POST, []() {
-    String message = server.arg("plain");
-    Serial.println("Message received: " + message);
-    server.send(200, "text/plain", "Message received");
-  });
+  // Set all motor control pins as outputs
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Define server routes
+  server.on("/send", HTTP_POST, handleMotorControl);
 
   server.begin();
 }
 
 void loop() {
   server.handleClient();
+}
+
+void handleMotorControl() {
+  String message = server.arg("plain");
+  Serial.println("Message received: " + message);
+
+  if (message == "forward") {
+    moveForward();
+    server.send(200, "text/plain", "Moving Forward");
+  } else if (message == "backward") {
+    moveBackward();
+    server.send(200, "text/plain", "Moving Backward");
+  } else if (message == "left") {
+    turnLeft();
+    server.send(200, "text/plain", "Turning Left");
+  } else if (message == "right") {
+    turnRight();
+    server.send(200, "text/plain", "Turning Right");
+  } else if (message == "stop") {
+    stopMotors();
+    server.send(200, "text/plain", "Stopping Motors");
+  } else {
+    server.send(400, "text/plain", "Invalid Command");
+  }
+}
+
+void moveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void moveBackward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void turnLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void turnRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
